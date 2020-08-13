186895c7513c80c1657d55183cdf7993ac851802
Thomas Allmer <d4kmor@gmail.com>
2020-08-13 16:05:48 +0200

    feat: add testing-runner learn for writing plugins

---
+++ learn/test-runner-writing-plugins/test/getUser.test.js
@@ -0,0 +1,10 @@
+import { expect } from "@open-wc/testing";
+import { getUser } from "../src/getUser.js";
+
+it("can get meta data via a userId", async () => {
+  const user3 = await getUser(3);
+  expect(user3.name).to.equal("Lea");
+
+  const user10 = await getUser(10);
+  expect(user10.name).to.equal("Peter");
+});
