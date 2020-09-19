dfb4d75b9793e9116871291149188ba0b4204751
Lars den Bakker <larsdenbakker@gmail.com>
2020-09-19 08:42:39 +0200

    Add example of serving test files

--- preact-jsx/web-test-runner.config.mjs
+++ preact-jsx/web-test-runner.config.mjs
@@ -3,6 +3,17 @@ import { esbuildPlugin } from "@web/dev-server-esbuild";
 export default {
   nodeResolve: true,
   plugins: [
+    {
+      serve(context) {
+        console.log('serve', context.path)
+      },
+      transform(context) {
+        console.log('transform', context.path)
+      },
+      resolveImport({ context  }) {
+        console.log('resolveImport', context.path)
+      },
+    },
     esbuildPlugin({ jsx: true, jsxFactory: "h", jsxFragment: "Fragment" }),
   ],
 };
