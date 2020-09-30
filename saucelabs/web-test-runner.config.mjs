cf32aa0a5b368eea435f1793122449c6ac08c43c
Lars den Bakker <larsdenbakker@gmail.com>
2020-09-30 21:56:47 +0200

    Run saucelabs tests

--- saucelabs/web-test-runner.config.mjs
+++ saucelabs/web-test-runner.config.mjs
@@ -1,5 +1,5 @@
-import { createSauceLabsLauncher } from '@web/test-runner-saucelabs';
-import { legacyPlugin } from '@web/dev-server-legacy';
+import { createSauceLabsLauncher } from "@web/test-runner-saucelabs";
+import { legacyPlugin } from "@web/dev-server-legacy";
 
 // configure the local Sauce Labs proxy, use the returned function to define the
 // browsers to test
@@ -9,49 +9,75 @@ const sauceLabsLauncher = createSauceLabsLauncher({
   user: process.env.SAUCE_USERNAME,
   key: process.env.SAUCE_ACCESS_KEY,
   // the Sauce Labs datacenter to run your tests on, defaults to 'us-west-1'
-  region: 'eu-central-1',
+  region: "eu-central-1",
 });
 
 const sharedCapabilities = {
-  'sauce:options': {
-    name: 'my test name',
+  "sauce:options": {
+    name: "my test name",
     // if you are running tests in a CI, the build id might be available as an
     // environment variable. this is useful for identifying test runs
     // this is for example the name for github actions
-    build: `my project ${process.env.GITHUB_REF ?? 'local'} build ${
-      process.env.GITHUB_RUN_NUMBER ?? ''
+    build: `my project ${process.env.GITHUB_REF ?? "local"} build ${
+      process.env.GITHUB_RUN_NUMBER ?? ""
     }`,
   },
 };
 
 export default {
-  concurrency: 3,
+  concurrentBrowsers: 2,
+  concurrency: 6,
   browserStartTimeout: 1000 * 30,
   sessionStartTimeout: 1000 * 30,
   sessionFinishTimeout: 1000 * 30,
   browsers: [
-    // create a browser launcher per browser you want to test
-    // you can get the browser capabilities from the Sauce Labs website
     sauceLabsLauncher({
       ...sharedCapabilities,
-      browserName: 'chrome',
-      browserVersion: 'latest',
-      platformName: 'Windows 10',
-    }),    
-
+      browserName: "chrome",
+      browserVersion: "latest",
+      platformName: "Windows 10",
+    }),
     sauceLabsLauncher({
       ...sharedCapabilities,
-      browserName: 'internet explorer',
-      browserVersion: '11.0',
-      platformName: 'Windows 7',
+      browserName: "chrome",
+      browserVersion: "latest-1",
+      platformName: "Windows 10",
+    }),
+    sauceLabsLauncher({
+      ...sharedCapabilities,
+      browserName: "firefox",
+      browserVersion: "latest",
+      platformName: "Windows 10",
+    }),
+    sauceLabsLauncher({
+      ...sharedCapabilities,
+      browserName: "safari",
+      browserVersion: "latest",
+      platformName: "macOS 10.15",
+    }),
+    sauceLabsLauncher({
+      ...sharedCapabilities,
+      browserName: "MicrosoftEdge",
+      browserVersion: "latest",
+      platformName: "Windows 10",
+    }),
+    sauceLabsLauncher({
+      ...sharedCapabilities,
+      browserName: "MicrosoftEdge",
+      browserVersion: "18",
+      platformName: "Windows 10",
+    }),
+    sauceLabsLauncher({
+      ...sharedCapabilities,
+      browserName: "internet explorer",
+      browserVersion: "11.0",
+      platformName: "Windows 7",
     }),
   ],
-  plugins: [
-    legacyPlugin()
-  ],
+  plugins: [legacyPlugin()],
   testFramework: {
     config: {
-      timeout: '10000',
+      timeout: "10000",
     },
-  }
+  },
 };
