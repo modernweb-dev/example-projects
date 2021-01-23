b47c9caadf4eb7c2e9e730c932cc4c201c0c9b4f
Lars den Bakker <larsdenbakker@gmail.com>
2021-01-23 17:16:24 +0100

    Add snowpack examples

---
+++ snowpack-svelte/snowpack.config.js
@@ -0,0 +1,28 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: {url: '/', static: true},
+    src: {url: '/dist'},
+  },
+  plugins: [
+    '@snowpack/plugin-svelte',
+    '@snowpack/plugin-dotenv',
+  ],
+  routes: [
+    /* Example: Enable an SPA Fallback in development: */
+    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+  ],
+  optimize: {
+    /* Example: Bundle your final build: */
+    // "bundle": true,
+  },
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
